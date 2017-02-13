b69d2d630009c7c8bafe720780d6e54d0bd471ea
VenkatPedapudi <vpedapudi@nisum.com>
2017-02-13 18:58:15 +0530

    Added password encrypter

--- IOTEventServices/event-data-service/src/main/java/com/semisol/data/dao/api/LoginDAO.java
+++ IOTEventServices/event-data-service/src/main/java/com/semisol/data/dao/api/LoginDAO.java
@@ -1,5 +1,7 @@
 package com.semisol.data.dao.api;
 
 public interface LoginDAO {
+	
+	void addUser();
 
 }
---
+++ IOTEventServices/event-data-service/src/main/java/com/semisol/data/util/PasswordUtil.java
@@ -0,0 +1,35 @@
+package com.semisol.data.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordUtil {
+	
+	
+	public static String getEncryptedPassword(String password) throws NoSuchAlgorithmException{
+		 
+		MessageDigest md = MessageDigest.getInstance("MD5");
+         //Add password bytes to digest
+         md.update(password.getBytes());
+         //Get the hash's bytes 
+         byte[] bytes = md.digest();
+         //This bytes[] has bytes in decimal format;
+         //Convert it to hexadecimal format
+         StringBuilder sb = new StringBuilder();
+         for(int i=0; i< bytes.length ;i++)
+         {
+             sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+         }
+         //Get complete hashed password in hex format
+        return sb.toString();
+	}
+	
+	public static boolean verifyPassword(String password,String savedPassword) throws NoSuchAlgorithmException{
+		
+		if(savedPassword.equals(getEncryptedPassword(password))) {
+		  return true;
+		} else
+	  	return false;
+	}
+
+}
